bb9e32486b947a09d8d81b6b5d98ae92cef6ff05
Aleff Alves <aleff.alves44@gmail.com>
2021-03-24 21:43:13 -0300

    adiciona aulas 01, 02, e 03

---
+++ aprendendo-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 75;
+        String nome = " Goku ";
+
+        System.out.println(idade);
+        System.out.println("A idade é " + idade + " anos!");
+        System.out.println(falso);
+        System.out.println(caractere);
+        System.out.println("Olá eu sou " + nome);
+    }
+}
---
+++ aprendendo-java/src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Aleff Alves";
+        String endereco = "Av Coronel Durval de Barros,";
+        double salario = 5431.12;
+        String dataRecebimentoSalario = "20/10/2021.";
+        String relatorio = "Eu "+nome+", morando no endereço, " +endereco+ " confirmo que recebi o sálario de R$" +salario+ ", na data " +dataRecebimentoSalario;
+        System.out.println(relatorio);
+
+    }
+}
