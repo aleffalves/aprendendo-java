2a44e1286b5d867b4c6cd380ccb8821abf4c7262
aleff <aleff.alves44@gmail.com>
2021-03-25 20:28:45 -0300

    "operadores"

---
+++ aprendendo-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,42 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.sql.SQLOutput;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int numero01 = 10;
+        int numero02 = 20;
+        System.out.println(numero01+numero02);
+        System.out.println("Valor "+numero02+numero01);
+        System.out.println(numero02+numero01+" Valor "+numero02+numero01);
+        int resultado = numero01 * numero02;
+        System.out.println(resultado);
+
+        //% resto
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        System.out.println(isDezMaiorQueVinte);
+        System.out.println(isDezMenorQueVinte);
+
+        // && (AND), || (or) !
+
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta "+isDentroDaLeiMenorQueTrinta);
+
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel "+isPlaystationCincoCompravel);
+
+    }
+}
